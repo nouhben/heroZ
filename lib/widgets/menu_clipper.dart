ce47c25cd861c5037679f9e09dad1f6c834f9a24
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-08 00:56:13 +0100

    final commit

---
+++ lib/widgets/menu_clipper.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MenuClipper extends CustomClipper<Path> {
+  double roundnessFactor = 50.0;
+  @override
+  getClip(Size size) {
+    var _path = Path();
+    _path.moveTo(0.0, 0.0);
+    _path.lineTo(0, size.width);
+    _path.quadraticBezierTo(0, size.height, roundnessFactor, size.height);
+    _path.lineTo(size.width - roundnessFactor, size.height);
+    _path.quadraticBezierTo(size.width, size.height, size.width, size.height - roundnessFactor);
+    _path.lineTo(size.width, roundnessFactor * 2);
+    _path.quadraticBezierTo(size.width, 0.0, size.width - roundnessFactor * 3, roundnessFactor * 2);
+    _path.lineTo(roundnessFactor, size.height * 0.33 + 10);
+    _path.quadraticBezierTo(
+      0,
+      size.height * 0.33 + roundnessFactor,
+      0,
+      size.height * 0.33 + roundnessFactor * 2,
+    );
+
+    return _path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    //throw UnimplementedError();
+    return true;
+  }
+}
